d403d0f415956e74260e1d05ae061f2b327a099e
sih3rron <siherron+github@gmail.com>
2023-07-19 11:20:38 +0100

    Dummy App for a demo

---
+++ src/app/components/Button.js
@@ -0,0 +1,11 @@
+import styles from "./styles/Button.css";
+
+export default function Button(){
+    return (
+        <>
+        <div className={styles.shape}>
+            Button
+        </div>
+        </>
+    )
+}
